7e9a1b3ae331f6788cbaa58b812648996d840087
wezd1308 <55217404+wezd1308@users.noreply.github.com>
2026-04-10 17:34:30 -0300

    Update file_cleaner_script.sh

--- file_cleaner_script/file_cleaner_script.sh
+++ file_cleaner_script/file_cleaner_script.sh
@@ -5,12 +5,12 @@ ARCHIVOS=$(find "$DIRECTORIO" -type f -size +1G)
 
 if  [ -n "$ARCHIVOS" ]; then
 
-    echo "Borrando archivos mayores a 1GB .."
+    echo "Borrando archivos mayores a 1GB ($(date))" >> log.txt
     rm -f "$ARCHIVOS"
      
     sleep 10
 
   else
 
-       echo "No Se encotraron archivos mayores a 1GB" 
+       echo "NO Se encotraron archivos mayores a 1GB ($(date))" >> log.txt
 fi
